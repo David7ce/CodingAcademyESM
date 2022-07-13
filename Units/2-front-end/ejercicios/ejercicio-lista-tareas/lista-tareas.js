34b88462b64184b1f4ee2e5f08b145293a3555af
David7ce <david7ce@outlook.com>
2022-07-13 11:30:16 +0100

    Clase js

--- Units/2-front-end/ejercicios/ejercicio-lista-tareas/lista-tareas.js
+++ Units/2-front-end/ejercicios/ejercicio-lista-tareas/lista-tareas.js
@@ -1,38 +1,83 @@
-// Definir una tarea, es un objeto con tres variables
-let tarea = {
-    nombre: "Fregar la losa",
-    descripcion: "Hay que fregar todos los platos de la casa",
-    completada: false, // "completada" "no completada"
-};
-
-// Lista de tareas es un array de objetos
+// Lista de tareas es un array de objetos, con nombre, descripción y estado de tarea (completada o no)
 let listaTareas = [
     {
         nombre: "Sacar al gato a paser",
-        descripcion: "",
+        descripcion: "descripción de scar al gato a pasear",
         completada: false,
     },
     {
-        nombre: "Sacar la ropa",
-        descripcion: "",
-        completada: true,
+        nombre: "Tender la ropa",
+        descripcion: "tender la ropa en el tendedero",
+        completada: false,
     },
     {
-        nombre: "Limpiar los platos",
-        descripcion: "",
+        nombre: "Fregar el suelo",
+        descripcion: "fregar el suelo con la fregona",
         completada: false,
-    }
+    },
+    {
+        nombre: "Limpiar el baño",
+        descripcion: "limpiar el baño de la casa",
+        completada: false,
+    },
 ];
 
-function mostrarTarea(tarea) {
-    return `[${tarea.completada ? "x" : " "}] ${tarea.nombre} (${tarea.descripcion})`
+// Definir una tarea, es un objeto con tres variables
+let tarea = {
+    nombre: "Fregar la losa",
+    descripcion: "hay que fregar todos los platos de la casa",
+    completada: false, // "completada" "no completada"
+};
+
+let tarea2 = {
+    nombre: "Tirar la basura",
+    descripcion: "",
+    completada: false, // "completada" "no completada" 
+};
+
+
+// Funciones
+function tareaATexto(tarea) {
+    return `[${tarea.completada ? "x" : " "}] ${tarea.nombre} (${tarea.descripcion})`;
 }
 
 function mostrarLista(lista) {
     for (let i = 0; i < lista.length; i++) {
-        console.log(mostrarTarea(lista[i]))
+        console.log(tareaATexto(lista[i]));
     }
 }
 
+function añadirTarea(lista, tarea) {
+    if (tarea.nombre !== undefined && tarea.descripcion !== undefined && tarea.completada !== undefined) {
+        lista.push(tarea);
+    }
+    else {
+        console.log("ERROR: La tarea no tiene las propiedades correctas.");
+    }
+}
+
+function completar(lista, nombreTarea) {
+    for (let i = 0; i < lista.length; i++) {
+        if(lista[i].nombre === nombreTarea) {
+            lista[i].completada = true;
+        }
+    }
+}
+
+
+añadirTarea(listaTareas, tarea);
+// añadirTarea(listaTareas, tarea2);
+añadirTarea(listaTareas, {
+    nombre: "Tarea  sin variables, es un objeto metido directamente",
+    descripcion: "funciona igual que las demás",
+    completada: "",
+});
+
+console.log("<!-- Antes de completar -->");
 mostrarLista(listaTareas);
-console.log(mostrarTarea(tarea));
+
+completar(listaTareas, "Tender la ropa");
+
+console.log("<!-- Después de completar -->");
+mostrarLista(listaTareas);
+
---
+++ Units/2-front-end/ejercicios/manipulando-arrays/manipulando-arrays.js
@@ -0,0 +1,8 @@
+let ejemplo = ["a", "b", "c", "d", "e", true];
+
+console.log(ejemplo.join());
+console.log(ejemplo.join(", "));
+console.log(ejemplo.join("\n"));
+
+console.log(ejemplo.reverse());
+console.log(ejemplo.reverse().join());
