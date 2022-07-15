7574921fb2e934e260b9a507fdc9d6747bd86577
David7ce <david7ce@outlook.com>
2022-07-15 13:56:48 +0100

    clase js

--- Units/2-front-end/ejercicios-js/ejemplo2-eventos/ejemplo2-eventos.js
+++ Units/2-front-end/ejercicios-js/ejemplo2-eventos/ejemplo2-eventos.js
@@ -11,9 +11,37 @@ function mostrarValor() {
     let indicador = document.getElementById("valor-rango");
     indicador.innerText = valor;  // mostrar texto en el documento HTML, dentro del id="valor-rango"
 }
+mostrarValor();
 
-function mostrarPalabra() {
-    
+function actualizarPalabra() {
+    let entrada = document.getElementById("palabra");
+    let valor = entrada.value;
+    let valorPalabra = document.getElementById("valor-palabra");
+    valorPalabra.innerText = valor;
 }
 
-mostrarValor();
+function tieneNumeros(password) {
+    let numeros = "1234567890".split("");
+    for (let i = 0; i < numeros.length; i++) {
+        if (password.includes(numeros[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function validarPassword() {
+    let password = document.getElementById("password").value;
+    let mensaje = document.getElementById("mensaje-password");
+    console.log(password.length);
+
+    if (password.length < 8 || !tieneNumeros(password)) {
+        mensaje.innerText = "Contraseña poco segura (menor a 8 caracteres o no contiene números).";
+    }
+    else {
+        mensaje.innerText = "Contraseña segura."; // no funciona
+        console.log("hola");
+    }
+}
+
+// Tutorial mover carpetas arrastrándolas
