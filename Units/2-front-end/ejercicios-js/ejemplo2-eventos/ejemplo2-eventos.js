3d8d2bc65df1b44e549da12c3c406e1598161da0
David7ce <david7ce@outlook.com>
2022-07-15 11:30:40 +0100

    subir cambios

--- Units/2-front-end/ejercicios-js/ejemplo2-eventos/ejemplo2-eventos.js
+++ Units/2-front-end/ejercicios-js/ejemplo2-eventos/ejemplo2-eventos.js
@@ -3,3 +3,17 @@ function mostrarConsola() {
     let valor = entrada.value;
     console.log(valor);
 }
+
+function mostrarValor() {
+    let entrada = document.getElementById("rango");
+    let valor = entrada.value; 
+    // console.log(valor); // mostrar el valor por consola
+    let indicador = document.getElementById("valor-rango");
+    indicador.innerText = valor;  // mostrar texto en el documento HTML, dentro del id="valor-rango"
+}
+
+function mostrarPalabra() {
+    
+}
+
+mostrarValor();
